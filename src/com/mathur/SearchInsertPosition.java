08cd749dfdad5ce4f46890fd868bc16d6247446c
Ankit <ankitmathur4u@gmail.com>
2021-09-19 08:44:09 +0100

    Day1

--- src/com/mathur/SearchInsertPosition.java
+++ src/com/mathur/SearchInsertPosition.java
@@ -1,4 +1,4 @@
-package com.mathur;
+/*Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.*/package com.mathur;
 
 public class SearchInsertPosition {
     public static int searchInsert(int[] nums, int target) {
